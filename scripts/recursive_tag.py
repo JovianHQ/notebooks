cf29c5006f909af145078c11ebbbea969f7353ee
VimYoung <bhaumik2386@gmail.com>
2025-11-25 17:46:02 +0530

    Add: Addition of scripts for adding run on colab tags in notebooks

---
+++ scripts/recursive_tag.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import subprocess
+from pathlib import Path
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Download a Jovian notebook (.ipynb)")
+    parser.add_argument("course_name", help="Public Jovian notebook URL")
+    args = parser.parse_args()
+    scripts_dir = Path(__file__).resolve().parent
+    file_path = str(scripts_dir.parent) + "/" + args.course_name
+    print(f"Script_path : {scripts_dir}/recursive_tag.py")
+    for assgn_courses in os.listdir(file_path):
+        sec_path = file_path + "/" + assgn_courses
+        for notebook in os.listdir(sec_path):
+            notebook_url = f"https://github.com/JovianHQ/notebooks/blob/main/{args.course_name}/{assgn_courses}/{notebook}"
+            print(notebook_url)
+            result = subprocess.run(
+                ["python", f"{scripts_dir}/capture_tag.py", notebook_url],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                env=os.environ.copy(),  # important for Firefox/Selenium
+            )
+            print("STDOUT:", result.stdout)
+            if result.stderr:
+                print("STDERR:", result.stderr)
+            if result.returncode != 0:
+                print(
+                    f"ERROR: capture_tag.py failed with return code {result.returncode}"
+                )
+            print(f"Done for {notebook_url}")
+
+
+if __name__ == "__main__":
+    main()
